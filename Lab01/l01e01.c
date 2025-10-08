59e01b40b18e23b95e37713dffaa22e8b5d763ff
5Eirals <emanuele.eirale@outlook.it>
2025-10-08 11:34:57 +0200

    checkMeta funcion - l01e01 - Laptop

--- Lab01/l01e01.c
+++ Lab01/l01e01.c
@@ -6,58 +6,109 @@
 //	\A trova un carattere maiuscolo
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-char* cercaRegexp(char* src, char* regexp);
+int cercaRegexp(char* src, char* regexp);
 int windowSize(char* str);
+int checkMeta(char c, char* regexp, int* regex_index);
 
 int main(){
 	char* src = "hello world";
     char* regexp = ".\\a[pqr]ld";
-    char* result = cercaRegexp(src, regexp);
+
+	printf("src: %s \n\tregex: %s\n", src, regexp);
+    int result = cercaRegexp(src, regexp);
+
+    if (result < 0){
+    	printf("Regex not found\n");
+    } else {
+    	printf("Regex found form index: %d = %c\n", result, src[result]);
+    }
 	return 0;
 }
 
-int windowSize(char* str){
-	int size = strlen(str);
-	printf("size regex: %d size world: %d\n", size, strlen("world"));
-	char* ptr, *start = str;
+int checkMeta(char c, char* regexp, int* regex_index){
+	printf("\nInside checkMeta, index: %d\n", *regex_index);
+	if(regexp[*regex_index] == '.') {
+		*regex_index = *regex_index + 1;
+		return 1;
+	}
+	if(regexp[*regex_index++] == '\\') {
+		if (regexp[*regex_index++] == 'a' && 'a'-1 < c && c < 'z'+1)
+			return 1;
+		if (regexp[*regex_index++] == 'A' && 'A'-1 < c && c < 'Z'+1)
+			return 1;
+	}
+	if(regexp[*regex_index++] == '[') {
+		if(regexp[*regex_index++] == '^') {
+			while (regexp[*regex_index] != ']') {
+				if (regexp[*regex_index++] == c)
+					return 0;
+			}
+		} else {
+			while (regexp[*regex_index] != ']') {
+				if (regexp[*regex_index++] == c)
+					return 1;
+			}
+		}
+	}
 
-    // while((ptr = strchr(start, '.')) != NULL){
-    // 	size--;
-    //     start = ptr + 1;
-    // 	printf("found . new size:%d \n", size);
-    // }
-
-    start = str;
-    while((ptr = strchr(start, '\\')) != NULL){
-        size--; // either \a or \A since src is alphanumerical
-        start = ptr + 1;
-
-    	printf("found \\a new size:%d \n", size);
-    }
-
-    start = str;
-    while((ptr = strchr(start, '[')) != NULL){
-    	char* ptrf;
-    	if((ptrf = strchr(start, ']')) != NULL){
-        	size =  size - (ptrf-ptr) + 1;
-                // considering src as alphanumerical the only occurence of [] is for a metacharacter
-        	start = ptrf + 1;
-    	}
-
-    	printf("found [] removing %d new size:%d \n", ptrf-ptr, size);
-    }
-
-    return size;
+	if(regexp[*regex_index++] == c)
+		return 1;
+    return 0;
 }
 
-char* cercaRegexp(char* src, char* regexp){
-	char* res = NULL;
-    char window[windowSize(regexp)];
+int cercaRegexp(char* src, char* regexp){
+	int i;
+	int flag = 1;
+    for(i = 0; i < strlen(src) || flag == 0; i++){
+        for(int j = 0; regexp[j] != '\0' || flag;){
+			flag = 0;
+        	printf("Inside for, checking: %c\n", regexp[j]);
+        	//traduci come switch
+        	if(regexp[j++] == '.'){
+        		continue;
+        	}
+        	if(regexp[j++] == '\\') {
+        		if (regexp[j++] == 'a' && !('a'-1 < src[i] && src[i] < 'z'+1)) {
+					flag = 1;
+        			break;
+        		}
+        		if (regexp[j++] == 'A' && !('A'-1 < src[i] && src[i] < 'Z'+1)) {
+					flag = 1;
+        			break;
+        		}
+        	}
+        	if(regexp[j++] == '[') {
+        		if(regexp[j++] == '^') {
+        			while (regexp[j++] != ']') {
+        				if (regexp[j++] == src[i]) {
+        					flag = 1;
+        				}
+        			}
+        			break;
+        		} else {
+        			while (regexp[j++] != ']') {
+        				flag = 1;
+        				if (regexp[j++] == src[i]) {
+        					flag = 0;
+        					break;
+        				}
+        			}
+        			break;
+        		}
+        	}
 
-
-    return res;
+        	if(regexp[j++] != src[i]) {
+        		flag = 1;
+        		break;
+        	}
+        }
+    }
+    if (flag) {
+	    return -1;
+    } else {
+	    return i;
+    }
 }
 
