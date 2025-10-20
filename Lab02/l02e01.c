e4c479f77e2e072a961674e1d85d094d03a5c524
Eirals <80176679+5Eirals@users.noreply.github.com>
2025-10-20 22:40:21 +0200

    completed l02e01 - Desktop

--- Lab02/l02e01.c
+++ Lab02/l02e01.c
@@ -7,19 +7,29 @@ int main(){
     printf("Insert the numbers: ");
     scanf("%d %d", &a, &b);
 
+    printf("gcd: %d", gcd(a,b));
+    return 0;
+}
+
+int gcd(int a, int b){
+
     if(b > a){
         b = a + b;
         a = b - a;
         b -= a;
     }
 
-    printf("gcd: %d", gcd(a,b));
-    return 0;
-}
+    if(a%b == 0)
+      return b;
 
-int gcd(int a, int b){
-    if()
-      return;
-
-    gcd();
+    if(a%2 == 0)
+    	if(b%2 == 0)
+        	return 2*gcd(a/2, b/2);
+    	else
+        	return gcd(a/2, b);
+    else
+    	if(b%2 == 0)
+        	return gcd(a, b/2);
+    	else
+        	return gcd((a-b)/2, b);
 }
