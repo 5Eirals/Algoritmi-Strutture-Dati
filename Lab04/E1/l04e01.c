1b65199baa959bdcf814033fd48c694d2e6e8ff8
5Eirals <emanuele.eirale@outlook.it>
2025-11-05 10:02:02 +0100

    completed l04e01 - Laptop

--- Lab04/E1/l04e01.c
+++ Lab04/E1/l04e01.c
@@ -3,15 +3,22 @@
 
 #define FILE_ERROR 3
 
+typedef enum {TRUE = 1, FALSE = 0} bool;
 
-void printMatrix(int** matrix, int row, int col);
-void printVertexCover(int** graph, int N, int E);
-void vertexCoverComb(int pos, int** mat, int* sol, int start, int N, int k);
-// rifare con powerset
+typedef struct {
+  int a;
+  int b;
+} Arch;
+
+void printVertexCover(Arch* arches, int* nodes, int N, int E);
+void vertexCoverPowerset(int pos, Arch* arches, int* nodes, int* sol, int start, int N, int E);
+bool checkCover(Arch* arches, int* nodes, int N, int E);
 int main() {
 	FILE* fin;
     int a, b;
 	int N, E;
+    int* nodes;
+    Arch* arches;
 
     if((fin = fopen("Lab04/E1/grafo.txt", "r")) == NULL) {
     	printf("An error occured while opening the input file, terminating ...\n");
@@ -19,65 +26,64 @@ int main() {
     }
 
     fscanf(fin, "%d %d", &N, &E);
-   	int **graph = (int**) malloc(N * sizeof(int*));
-    for (int i = 0; i < N; i++) {
-        graph[i] = (int*) calloc(N, sizeof(int));
+
+   	nodes = (int*) malloc(N * sizeof(int));
+    for(int i = 0; i < N; i++) {
+    	nodes[i] = i;
     }
 
+    arches = (Arch*) malloc(E * sizeof(Arch));
     for (int i = 0; i < E; i++) {
     	if(fscanf(fin, "%d %d", &a, &b) == 2){
-    		graph[a][b] = 1;
+    		arches[i].a = a;
+            arches[i].b = b;
     	}
     }
 
-    printMatrix(graph, E, N);
-	printVertexCover(graph, N, E);
+	printVertexCover(arches, nodes, N, E);
 
     fclose(fin);
-	for (int i = 0; i < E; i++) {
-		free(graph[i]);
-	}
-    free(graph);
+	free(arches);
+    free(nodes);
 	return 0;
 }
 
-void printMatrix(int** matrix, int row, int col) {
-	for (int i = 0; i < row; i++) {
-          for (int j = 0; j < col; j++) {
-            	printf("%d ", matrix[i][j]);
-          }
-          printf("\n");
-	}
+
+void printVertexCover(Arch* arches, int* nodes, int N, int E) {
+	int* sol = malloc(sizeof(int) * N);
+	vertexCoverPowerset(0, arches, nodes, sol, 0, N, E);
+	free(sol);
 }
 
-void printVertexCover(int** graph, int N, int E) {
-	for (int i = 2; i <= E; i++) {
-		int* sol = malloc(sizeof(int) * i);
-		int* mark = calloc(N, sizeof(int));
-		printf("Verterx cover for k:%d\n", i);
-		vertexCoverComb(0, graph, sol, 0, N, i);
-		free(sol);
-	}
-}
-
-void vertexCoverComb(int pos, int** mat, int* sol, int start, int n, int k){
-	printf("Inside vertexCoverComb\n");
-	if(pos >= k){
-		printf("(");
-		for(int i = 0; i < k; i++) printf("%d ",sol[i]);
-		printf(")\n");
-		return;
-	}
-
-	for(int i = start; i < n; i++){
-		for(int j = 0; j < n; j++) {
-			if (mat[i][j] == 1) {
-				printf("Match (%d, %d)\n sol: ", i, j); for(int i = 0; i < k; i++) printf("%d ",sol[i]); printf("\n");
-				sol[pos] = i;
-				vertexCoverComb(pos+1, mat, sol, j, n, k);
+bool checkCover(Arch* arches, int* nodes, int N, int E) {
+	for (int i = 0; i < E; i++) {
+		bool cover = FALSE;
+		for (int j = 0; j < N; j++) {
+			if (arches[i].a == nodes[j] || arches[i].b == nodes[j]) {
+				cover = TRUE;
+				break;
 			}
 		}
-
+		if (cover == FALSE)
+			return FALSE;
 	}
+
+	return TRUE;
 }
 
+void vertexCoverPowerset(int pos, Arch* arches, int* nodes, int* sol, int start, int n, int e){
+	if (start >= n){
+		if (checkCover(arches, sol, pos, e)) {
+			for (int i = 0; i < pos; i++) printf("%d", sol[i]);
+			printf(" ");
+		}
+		return;
+	}
+	for (int i = start; i < n; i++){
+		sol[pos] = nodes[i];
+		vertexCoverPowerset(pos + 1, arches, nodes, sol, i+1, n, e);
+	}
+
+	vertexCoverPowerset(pos, arches, nodes, sol, n, n, e);
+	return;
+}
